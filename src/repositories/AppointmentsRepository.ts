6d6ec4a2961154e5376aeeb3354c5aca995559e0
Adolpho Cavalcanti <adolpho.nascimento@compasso.com.br>
2020-11-14 20:19:11 -0300

    first commit primeiro proj node bootcamp 14

---
+++ src/repositories/AppointmentsRepository.ts
@@ -0,0 +1,36 @@
+import Appointment from '../models/Appointment';
+import { isEqual } from 'date-fns';
+
+interface CreateAppointmentDTO {
+  provider: string;
+  date: Date;
+}
+
+class AppointmentsRepository {
+  private appointments: Appointment[];
+
+  constructor() {
+    this.appointments = [];
+  }
+
+  public findBydate(date: Date): Appointment | null {
+    const findAppointment = this.appointments.find(appointment =>
+      isEqual(date, appointment.date),
+    );
+    return findAppointment || null;
+  }
+
+  public getAll(): Appointment[] {
+    return this.appointments;
+  }
+
+  public create({ provider, date }: CreateAppointmentDTO): Appointment {
+    const appointment = new Appointment({ provider, date });
+
+    this.appointments.push(appointment);
+
+    return appointment;
+  }
+}
+
+export default AppointmentsRepository;
